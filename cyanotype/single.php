d687c134f16650cf1961f34a2014595e628f52c9
gabeshaughnessy <gabeshaughnessy@gmail.com>
2012-04-02 17:57:57 -0700

    added single post template for modals

---
+++ cyanotype/single.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The Loop
+ *
+ */ 
+if ( have_posts() ) :  while( have_posts() ) : the_post(); ?>
+		<article id="post-<?php the_ID();?>" class="">
+		<div class="modal-header">
+		  <a class="close" data-dismiss="modal">×</a>
+		  <h3><?php the_title(); ?></h3>
+		</div>
+			<div class="entry-content modal-body">
+				<?php the_content(); ?>
+			</div>
+			<div class="modal-footer">
+			</div>
+		</article>
+		
+		<?php endwhile; ?>		
+		<?php endif; // end of the loop ?>
