7829f0727aa0340169dab0f0d973e37b87560681
gabeshaughnessy <gabeshaughnessy@gmail.com>
2012-04-15 15:35:21 -0700

    removed page width and height, scaled up sculpture, added factoid

--- cyanotype/index.php
+++ cyanotype/index.php
@@ -1,6 +1,6 @@
 <?php 
 get_header();
-//get_template_part('content_block', 'factoids');
+get_template_part('content_block', 'factoids');
 get_template_part('content_block', 'splash');
 
 get_footer();
